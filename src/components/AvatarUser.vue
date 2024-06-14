018a91d5ba1422d299ba186cc9c0bb8f303d726b
trungnt <trungptit7@gmail.com>
2024-06-14 14:55:41 +0700

    update

--- src/components/AvatarUser.vue
+++ src/components/AvatarUser.vue
@@ -4,15 +4,18 @@
       <img slot="reference" :src="urlAvatar" :style="styles" />
     </div>
     <el-dropdown-menu slot="dropdown" class="menu-user">
-      <router-link to="/survey-question" v-if="user.role == UserRole.Candidate">
+      <router-link
+        to="/survey-question"
+        v-if="user.role == UserRole.Candidate && !user.is_complete_survey"
+      >
         <el-dropdown-item>{{ $t("Survey Question") }}</el-dropdown-item>
       </router-link>
       <router-link to="/analysic" v-if="user.role == UserRole.Admin">
         <el-dropdown-item>{{ $t("Manager") }}</el-dropdown-item>
       </router-link>
-      <el-dropdown-item @click.native="settingProfile"
-        >{{ $t("Settings") }}</el-dropdown-item
-      >
+      <el-dropdown-item @click.native="settingProfile">{{
+        $t("Settings")
+      }}</el-dropdown-item>
       <el-dropdown-item divided @click.native="logout">
         <span style="display: block">{{ $t("Log Out") }}</span>
       </el-dropdown-item>
--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -1,29 +1,43 @@
 <template>
-  <div class="svelte-header" style="position: sticky; top: 0; z-index: 2">
-    <div
-      class="flex items-center justify-between bg-[#232E4D] lg:p-4 p-2"
-      style="padding: 12.5px; height: 79px"
-    >
-      <img
-        src="@/assets/images/logo-text.png"
-        style="cursor: pointer; object-fit: contain; height: 100%"
-        @click="$router.push({ path: '/' })"
-      />
-      <div class="nav-header py1 px2 flex items-center">
-        <MultipleLanguage style="margin-right: 15px" />
-        <AvatarUser :size="40" @settingProfile="settingProfile" />
+  <div>
+    <div class="svelte-header" style="position: sticky; top: 0; z-index: 2">
+      <div
+        class="flex items-center justify-between bg-[#232E4D] lg:p-4 p-2"
+        style="padding: 12.5px; height: 79px"
+      >
+        <img
+          src="@/assets/images/logo-text.png"
+          style="cursor: pointer; object-fit: contain; height: 100%"
+          @click="$router.push({ path: '/' })"
+        />
+        <div class="nav-header py1 px2 flex items-center">
+          <MultipleLanguage style="margin-right: 15px" />
+          <AvatarUser :size="40" @settingProfile="settingProfile" />
+        </div>
       </div>
     </div>
+    <el-dialog
+      :close-on-click-modal="false"
+      title="Settings Profile"
+      :visible.sync="isDisplaySettingsProfile"
+      top="5vh"
+    >
+      <SettingsProfile
+        v-if="isDisplaySettingsProfile"
+        @closeDialog="isDisplaySettingsProfile = false"
+      />
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import AvatarUser from "@/components/AvatarUser.vue";
 import MultipleLanguage from "@/layout/components/MultipleLanguage.vue";
+import SettingsProfile from "@/components/SettingsProfile.vue";
 export default {
-  components: { AvatarUser, MultipleLanguage },
+  components: { AvatarUser, MultipleLanguage, SettingsProfile },
   data() {
-    return {};
+    return { isDisplaySettingsProfile: false };
   },
   computed: {
     user() {
@@ -36,7 +50,7 @@ export default {
       this.$router.push(`/login?redirect=${this.$route.fullPath}`);
     },
     settingProfile() {
-      this.$emit("settingProfile");
+      this.isDisplaySettingsProfile = true;
     },
   },
 };
